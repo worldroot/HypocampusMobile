f57c6be99395a5e5841d8f194ae69caf2884fac8
Mehdi Behira <mehdibehira@gmail.com>
2020-04-26 12:06:51 +0100

    fix

--- src/com/mycompany/myapp/models/Backlog.java
+++ src/com/mycompany/myapp/models/Backlog.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.models;
+package com.mycompany.myapp.models;
 
 /**
  *
